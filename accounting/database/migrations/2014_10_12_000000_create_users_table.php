7cfc05c6da4308958d0fb38e302310e7c8392ab8
Md. Jahirul Islam <90275453+jahir002201@users.noreply.github.com>
2024-03-12 15:32:48 +0600

    migration
    Laravel migration code

---
+++ accounting/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Check if the users table does not exist
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->string('image', 255)->nullable();
+                $table->string('skill', 255);
+                $table->string('address', 255);
+                $table->unsignedBigInteger('role_id');
+                $table->rememberToken();
+                $table->timestamps();
+                $table->foreign('role_id')->references('id')->on('roles');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
